5738d72ba95f74b30a62e28b25ecde5eee9d8d13
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 11:03:33 +0900

    build: bump runtime config fix to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionCode = 2
+        versionName = "0.1.1-runtimefix"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
