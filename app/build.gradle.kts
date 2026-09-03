11b7398577046aeb58049e6f1b0714d6b488d0b3
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 20:39:49 +0900

    build: bump RRBOX HEV beta2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.2.0-beta1"
+        versionCode = 12
+        versionName = "0.2.0-beta2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
