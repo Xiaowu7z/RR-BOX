95879818803729ab6871114539fa410a0e8aee87
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 12:33:29 +0900

    build: bump Android interface fix to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3-dnsfix"
+        versionCode = 5
+        versionName = "0.1.4-netfix"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
