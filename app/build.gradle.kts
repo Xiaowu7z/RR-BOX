590c9432cca73348c39cb20cd925af68db37953d
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 13:17:46 +0900

    build: bump node tools and notification fix to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.4-netfix"
+        versionCode = 6
+        versionName = "0.1.5-node-tools"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
