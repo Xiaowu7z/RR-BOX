737b582826f56f0ca4275705bd7ea10de73f5214
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 13:44:44 +0900

    build: bump RRBOX routing release to 0.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.5-node-tools"
+        versionCode = 7
+        versionName = "0.1.6-rrbox-routing"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
