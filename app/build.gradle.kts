8bd618745ee05968ec52f9f2cf17959ce09bb2a5
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 15:51:56 +0900

    build: bump stability rollback to 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.7-smart-routing"
+        versionCode = 9
+        versionName = "0.1.8-stability"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
