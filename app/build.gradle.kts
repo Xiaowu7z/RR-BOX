6070feeb4b5a41451afb11b8154049a355897528
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 18:04:07 +0900

    build: release RRBOX 0.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.1.8-stability"
+        versionCode = 10
+        versionName = "0.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
