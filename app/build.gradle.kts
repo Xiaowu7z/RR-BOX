786816d90eb292d351313b920fd0366dfd8971e3
Xiaowu7z <moan7zai@gmail.com>
2026-09-03 11:43:34 +0900

    build: bump direct connectivity fix to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rr.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1-runtimefix"
+        versionCode = 3
+        versionName = "0.1.2-connectivity"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
